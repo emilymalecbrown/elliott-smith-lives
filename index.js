c32364715ba41ba8ddfb87a57967885f4f4929cd
emilymalecbrown <emilymalecbrown@gmail.com>
2017-02-20 10:59:38 -0500

    heroku stuff

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const twitterPackage = require('twitter');
 const keys = require('./keys');
 const findLyrics = require('./markov').generator;
 const initial = require('./markov').initialResult;
+const herokuKeys = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_KEY,
+  access_token_secret: process.env.ACCESS_SECRET
+}
 
-const Twitter = new twitterPackage(keys);
+const Twitter = process.env.NODE_ENV === 'production' ? new twitterPackage(herokuKeys) : new twitterPackage(keys);
 
 Twitter.post('statuses/update', {status: findLyrics(initial)},  function(error, tweet, response){
   if(error){
